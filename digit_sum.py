bf7c11afee96c0f94c6cdacdccd6f3f65252c678
Frank <fatukunda@gmail.com>
2018-08-28 10:21:02 +0300

    Testing if a given number is even

--- digit_sum.py
+++ digit_sum.py
@@ -6,15 +6,28 @@
 #     print(total)
     
 # digit_sum(123)
+
 """ Calculating factorial of a number"""
-def factorial(x):
-    if x == 1:
-        return 1
-    elif x == 0:
-        return 0
+# def factorial(x):
+#     if x == 1:
+#         return 1
+#     elif x == 0:
+#         return 0
+#     else:
+#         factorial = 1
+#         for number in range(1, x+1):
+#             factorial *= number
+#         return factorial
+
+
+""" Testing if a given number is a prime number"""
+def is_prime(x):
+    if x<2:
+        return False
     else:
-        factorial = 1
-        for number in range(1, x+1):
-            factorial *= number
-        return factorial
+         for n in range(2, x-1 ):
+            if x % n == 0:
+                return False
+            else:
+                return True
 
